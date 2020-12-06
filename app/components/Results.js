d0d0827297d43a4b685b725eadc096ddab11d6e8
Bryson Fraley <bryson.fraley@bluetent.com>
2020-12-06 10:49:06 -0700

    finished react course ui.dev github battle

---
+++ app/components/Results.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import {battle} from "../utils/api";
+import {FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser} from "react-icons/fa";
+import Card from "./Card";
+import PropTypes from 'prop-types';
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
+import queryString from 'query-string';
+import { Link } from 'react-router-dom'
+
+function ProfileList ({ profile }) {
+    return (
+        <ul className='card-list'>
+            <li>
+                <FaUser color='rgb(239, 115, 115)' size={22} />
+                {profile.name}
+            </li>
+            {profile.location && (
+                <li>
+                    <Tooltip text="User's location">
+                        <FaCompass color='rgb(144, 115, 255)' size={22} />
+                        {profile.location}
+                    </Tooltip>
+                </li>
+            )}
+            {profile.company && (
+                <li>
+                    <Tooltip text="User's company">
+                        <FaBriefcase color='#795548' size={22} />
+                        {profile.company}
+                    </Tooltip>
+                </li>
+            )}
+            <li>
+                <FaUsers color='rgb(129, 195, 245)' size={22} />
+                {profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+                <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+                {profile.following.toLocaleString()} following
+            </li>
+        </ul>
+    )
+}
+
+ProfileList.propTypes = {
+    profile: PropTypes.object.isRequired
+}
+
+export default class Results extends React.Component {
+    state = {
+        winner: null,
+        loser: null,
+        error: null,
+        loading: true
+    };
+
+    componentDidMount() {
+        const { playerOne, playerTwo } = queryString.parse(this.props.location.search);
+
+        battle([playerOne, playerTwo])
+            .then((players) => {
+                this.setState({
+                    winner: players[0],
+                    loser: players[1],
+                    error: null,
+                    loading: false,
+                });
+            }).catch(({message}) => {
+                this.setState({
+                    error: message,
+                    loading: false,
+                });
+            });
+    }
+
+    render() {
+        const {winner, loser, error, loading} = this.state;
+
+        if (loading === true) {
+            return <Loading />
+        }
+
+        if (error) {
+            <p className='center-text error'>{error}</p>
+        }
+
+        return (
+            <React.Fragment>
+                <div className='grid space-around container-sm'>
+                    <Card
+                        href={winner.profile.html_url}
+                        avatar={winner.profile.avatar_url}
+                        name={winner.profile.login}
+                        subheader={`Score: ${winner.score.toLocaleString()}`}
+                        header={winner.score === loser.score ? 'Tie' : 'Winner'}
+                    >
+                        <ProfileList profile={winner.profile} />
+                    </Card>
+                    <Card
+                        href={loser.profile.html_url}
+                        avatar={loser.profile.avatar_url}
+                        name={loser.profile.login}
+                        subheader={`Score: ${loser.score.toLocaleString()}`}
+                        header={winner.score === loser.score ? 'Tie' : 'Loser'}
+                    >
+                        <ProfileList profile={loser.profile} />
+                    </Card>
+                </div>
+                <Link
+                    className='btn dark-btn btn-space'
+                    to='/battle'
+                >
+                    Reset
+                </Link>
+            </React.Fragment>
+        )
+    }
+}
+
+Results.propTypes = {
+    playerOne: PropTypes.string.isRequired,
+    playerTwo: PropTypes.string.isRequired,
+}
